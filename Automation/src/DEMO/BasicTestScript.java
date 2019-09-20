7a3e93287735e36b1b6185ec818cb16fb2464898
admin <admin@192.168.0.118>
2019-09-20 17:51:32 +0530

    first commit

---
+++ Automation/src/DEMO/BasicTestScript.java
@@ -0,0 +1,14 @@
+package DEMO;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BasicTestScript {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.setProperty("webDriver.gecko.driver","C:\\driver\\geckodriver.exe");
+
+FirefoxDriver ff=new FirefoxDriver();
+	}
+
+}
---
+++ Automation/src/DEMO/Sample.java
@@ -0,0 +1,14 @@
+package DEMO;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
